c17bc308de3b99acf19b94e3d2aa8bfbfaa89c21
tomato23d <anna.a.art@gmail.com>
2023-06-05 15:46:28 +1000

    review post route

---
+++ public/scripts/add-article.js
@@ -0,0 +1,18 @@
+document.getElementById("the-form").addEventListener("click", (e) => {
+    e.preventDefault();
+    const post = {
+    title: document.getElementById("title").value.trim(),
+    text:  document.getElementById("text").value.trim(),
+    };
+    
+    fetch('/', {
+method: 'POST',
+headers: {'Content-Type': 'application/json',},
+body: JSON.stringify(post)})
+.then((res) => res.json())
+.then((data) => {console.log('Successful POST Review request:', data);
+  return data;
+})
+.catch((error) => {console.error('Error in POST request:', error);
+});
+})
--- readme.txt
+++ readme.txt
@@ -1,4 +1,9 @@
 
-checking form still produces error
+data entry on a form:
+
+seems a conflict b/w script on handlebar and public>script>js file
+
+=> disabled script; post route seems fine on homeRoutes;
+consider moving form to homepage (rather then article page since post route renders homepage (???))
 
 
